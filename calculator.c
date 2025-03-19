36c30dd7acfc8e2ab5c4dd4fa2893bf2589e6f39
Rachitha29 <rachitha.n@s.amity.edu>
2025-03-19 09:28:41 +0530

    Added addition and subtraction code

--- calculator.c
+++ calculator.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 int main()
 {
-	int a,b;
+	int a,b,add,subtract,mult,div;
 	printf("Enter a number");
 	scanf("%d",&a);
 	printf("Enter second number");
 	scanf("%d",&b);
-
+        add=a+b;
+	subtract=a-b;
